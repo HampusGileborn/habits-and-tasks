717b68c48ce3fe92caee75308eebfee0cbeb1b4e
HampusGileborn <h@gileborn.se>
2023-12-15 11:13:03 +0100

    Setup Home path

---
+++ src/Pages/NewHabit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewHabit() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NewHabit;
---
+++ src/Pages/NewTask.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NewTask() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NewTask;
+

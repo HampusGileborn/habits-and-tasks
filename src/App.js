df7618e7dddec8e1dc95ebe068c89e93e82e543a
HampusGileborn <h@gileborn.se>
2023-12-17 23:49:25 +0100

    New task funktion och skriva ut senaste tre task på startsidan

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import './App.css';
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
-import Home from './Pages/Home';
-import Tasks from './Pages/Tasks';
-import NewTask from './Pages/NewTask';
-import Habits from './Pages/Habits';
-import NewHabit from './Pages/NewHabit';
-import Friends from './Pages/Friends';
+import Home from "./Pages/Home";
+import Tasks from "./Pages/Tasks";
+import NewTask from "./Pages/NewTask";
+import Habits from "./Pages/Habits";
+import NewHabit from "./Pages/NewHabit";
+import Friends from "./Pages/Friends";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks([
+      {
+        id: 1,
+        titel: "Städa",
+        description: "plocka undan kläder och damsug",
+        time: 30 + " minuter",
+        type: "hushållssysslor",
+      },
+      {
+        id: 2,
+        titel: "Träna",
+        description: "Gå till gymmet och kör ben",
+        time: 100 + " minuter",
+        type: "aktivitet",
+      },
+      {
+        id: 3,
+        titel: "Plugga",
+        description: "Kolla på YouTube",
+        time: 2 + " minuter",
+        type: "plugg",
+      },
+      {
+        id: 4,
+        titel: "Plugga",
+        description: "Skriv kod",
+        time: 30 + " minuter",
+        type: "plugg",
+      }
+    ]);
+  }, []);
+
   return (
     <div>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/Tasks' element={<Tasks/>}/>
-        <Route path='/NewTask' element={<NewTask/>}/>
-        <Route path='/Habits' element={<Habits/>}/>
-        <Route path='/NewHabit' element={<NewHabit/>}/>
-        <Route path='/Friends' element={<Friends/>}/>
+        <Route path="/" element={<Home tasks={tasks}/>} />
+        <Route path="/Tasks" element={<Tasks />} />
+        <Route path="/NewTask" element={<NewTask tasks={tasks} setTasks={setTasks}/>} />
+        <Route path="/Habits" element={<Habits />} />
+        <Route path="/NewHabit" element={<NewHabit />} />
+        <Route path="/Friends" element={<Friends />} />
       </Routes>
     </div>
   );

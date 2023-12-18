fad1cb5788d57f0e4db5d893ee898b5b80f99c07
jko.ridder <jko.ridder@gmail.com>
2023-12-18 13:26:51 +0100

    Gemensam uppdatering av kod, ändrat i Home.jsx.

--- src/Pages/Habits.jsx
+++ src/Pages/Habits.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import '../App.css';
+
+
+
+
 
 function Habits() {
-  return (
-    <div>
-      <h3>This is a test</h3>
-      <p>All Habits</p>
 
+  const arrayAllHabits = allHabits.map((habit) => (
+    <li key={habit.Title} className='habitItem'>
+      <p>Title: {habit.Title}</p>
+      <p>Days done habit: {habit.streakValue} days in a row</p><button> reset streak </button><button> add one</button>
+      <p>Priority: {habit.priority}</p>
+    </li>
+  ));
+  
+  return (
+
+    <div className='habitPage'>
+      <h3>This is a test</h3>
+      <p>All Habits</p> 
+      <p>Filter</p><select> 
+        <option value="Low">Low</option>
+        <option value="Medium">Medium</option>
+        <option value="High">High</option>
+        <option value="">Show All</option>
+      </select>
+      <ul>{arrayAllHabits}</ul>
     </div>
   )
 }
--- src/Pages/NewHabit.jsx
+++ src/Pages/NewHabit.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-function NewHabit() {
+function NewHabit(props) {
   return (
-    <div>
+    <div className='formHabit'>
       <p>Create new Habit</p>
+      <form action="">
+        <p>Habit Title </p>
+        <input type="text" value="" placeholder="Title of the Habit"/>
+        <div>
+        <input type="number" value="" placeholder="Streak of the habit"/>
+        </div>
+        <select>
+          <option value="" disabled hidden>Change Priority</option> 
+          <option value="Low Priority">Low Priority</option>
+          <option value="Medium Priority">Medium Priority</option>
+          <option value="High Priority">High Priority</option>
+        </select>
+        <button>Add Habit</button>
+      </form>
     </div>
   )
 }

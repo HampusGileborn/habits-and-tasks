c869f5c266d7b5b1ca98173551e9b555284e0573
jko.ridder <jko.ridder@gmail.com>
2023-12-19 14:48:41 +0100

    Fixed formatting & added some css for Habits page

--- src/Pages/Habits.jsx
+++ src/Pages/Habits.jsx
@@ -14,36 +14,37 @@ function Habits(props) {
     });
     props.setHabits(updatedHabits);
   };
-const decreaseStreak = (id) => {
-  const updatedHabits = props.habits.map((habit) => {
-    if (habit.id === id) {
-      return {
-        ...habit,
-        streakValue: habit.streakValue - 1,
-      };
-    }
-    return habit;
-  });
-  props.setHabits(updatedHabits);
-};
+  const decreaseStreak = (id) => {
+    const updatedHabits = props.habits.map((habit) => {
+      if (habit.id === id) {
+        return {
+          ...habit,
+          streakValue: habit.streakValue - 1,
+        };
+      }
+      return habit;
+    });
+    props.setHabits(updatedHabits);
+  };
 
-const resetStreak = (id) => {
-  const confirmed = window.confirm("Are you sure you want to reset your streak? This cannot be undone.");
-  if (confirmed) {
-  const updatedHabits = props.habits.map((habit) => {
-    if (habit.id === id) {
-      return {
-        ...habit,
-        streakValue: 0,
-      };
+  const resetStreak = (id) => {
+    const confirmed = window.confirm(
+      "Are you sure you want to reset your streak? This cannot be undone."
+    );
+    if (confirmed) {
+      const updatedHabits = props.habits.map((habit) => {
+        if (habit.id === id) {
+          return {
+            ...habit,
+            streakValue: 0,
+          };
+        }
+        return habit;
+      });
+      props.setHabits(updatedHabits);
     }
-    return habit;
-  });
-  props.setHabits(updatedHabits);
-};
-}
-    
-  
+  };
+
   return (
     <>
       {props.habits.map((habits) => (
@@ -54,19 +55,21 @@ const resetStreak = (id) => {
             <button onClick={() => decreaseStreak(habits.id)}>-1</button>
             <button onClick={() => resetStreak(habits.id)}>Reset</button>
             <button onClick={() => increaseStreak(habits.id)}>+1</button>
+
             <h2>
               {" "}
               {habits.priority === 1
                 ? "Low"
                 : habits.priority === 2
                 ? "Medium"
-                : "High"} Priority
+                : "High"}{" "}
+              Priority
             </h2>
           </div>
         </>
       ))}
     </>
   );
-              }
+}
 
 export default Habits;
